6050c880113c1106d1bd761ce96deb16632c3c7a
adocsokkente <adocsokkente@freemail.hu>
2013-05-27 00:35:22 +0300

    Update arguments.h

--- Arguments/arguments.h
+++ Arguments/arguments.h
@@ -1,15 +1,26 @@
 #ifndef ARGUMENTS_H
 #define ARGUMENTS_H
 
+
+/** Argumentum enumeráció */
+
 enum Arguments
 {
+    ///Maximális életpont
     MaxHP = 0,
+    ///Jelenlegi életpont
     CurHP,
+    ///Maximális Force-pont
     MaxFP,
+    ///Jelenlegi Force-pont
     CurFP,
+    ///Erő (fizikai) pont
     STR,
+    ///Védekezés pont
     DEF,
+    ///Gyorsaság pont
     SPD,
+    ///Argumentumok száma
     NUMBER_OF_ARGUMENTS
 };
 

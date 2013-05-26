cb1eab861ab8979e2c11aa5e72e893d6849eb5b6
adocsokkente <adocsokkente@freemail.hu>
2013-05-26 23:32:40 +0200

    Create arguments.h

---
+++ Arguments/arguments.h
@@ -0,0 +1,16 @@
+#ifndef ARGUMENTS_H
+#define ARGUMENTS_H
+
+enum Arguments
+{
+    MaxHP = 0,
+    CurHP,
+    MaxFP,
+    CurFP,
+    STR,
+    DEF,
+    SPD,
+    NUMBER_OF_ARGUMENTS
+};
+
+#endif // ARGUMENTS_H

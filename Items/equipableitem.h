7841a99294c2daf02aea82b706bba24112e8adf0
adocsokkente <adocsokkente@freemail.hu>
2013-05-27 15:18:13 +0300

    Update equipableitem.h

--- Items/equipableitem.h
+++ Items/equipableitem.h
@@ -6,7 +6,7 @@
 #include "item.h"
 #include "arguments.h"
 
-/** Felszerelhető tárgy osztály */
+/** Felszerelhetõ tárgy osztály */
 
 class EquipableItem : public Item
 {
@@ -15,19 +15,23 @@ protected:
     int Modifier [NUMBER_OF_ARGUMENTS];
 public:
     ///Konstruktor
-    EquipableItem(std::string n = std::string(), std::string d = std::string(), bool s = false):Item(n,d,s)
+    EquipableItem(std::string n = std::string(),    ///Név
+                  std::string d = std::string(),    ///Leírás
+                  bool s = false)                   ///Eladhatóság
+                  :Item(n,d,s)                      ///Alaposztály konstruktorának meghívása
     {
         ///Alapértelmezetten 0-ba állítjuk az összes módosítót
         for (int i=0; i<NUMBER_OF_ARGUMENTS; i++)
             Modifier[i]=0;
     }
-    ///Módosító beállítása
+
+    ///Módosító beállítása - Kivételkezeléssel
     virtual void setModifier(int val, unsigned arg) {if(arg>=NUMBER_OF_ARGUMENTS)throw "Unaccessable memory"; Modifier[arg]=val;}
-    ///Módosító lekérdezése
+    ///Módosító lekérdezése - Kivételkezeléssel
     virtual const int& getModifier(unsigned arg) const {if(arg>=NUMBER_OF_ARGUMENTS)throw "Unaccessable memory"; return Modifier[arg];}
-    ///Hozzáadás adott módosító értékéhez
+    ///Hozzáadás adott módosító értékéhez - Kivételkezeléssel
     virtual void AddToModifier(int val, unsigned arg) {if(arg>=NUMBER_OF_ARGUMENTS)throw "Unaccessable memory"; Modifier[arg]+= val;}
     ///Destruktor
-    virtual ~EquipableItem() {}
+    virtual ~EquipableItem() { /** Itt semmit nem kell csinálni */ }
 };
 #endif // EQUIPABLEITEM_H

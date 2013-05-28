da6ea139dd634ad293f931945df881e23d41cf47
adocsokkente <adocsokkente@freemail.hu>
2013-05-29 01:27:34 +0300

    Update item.h

--- Items/item.h
+++ Items/item.h
@@ -1,6 +1,9 @@
 #ifndef ITEM_H
 #define ITEM_H
-#include <iostream>
+#include <string>
+#include <SDL_image.h>
+
+#include "image.h"
 
 /** Tárgy alaposztály */
 
@@ -13,11 +16,14 @@ protected:
     std::string description;
     ///Tárgy eladható-e
     bool sellable;
+    ///Tárgyak képére mutató Image struktúra
+    const static Image img;
 public:
     ///Konstruktor
     Item(std::string n = std::string(), ///Név
          std::string d = std::string(), ///Leírás
-         bool s = false)                ///Eladhatóság
+         bool s = false,                ///Eladhatóság
+         SDL_Surface* p = NULL)         ///Mutató a képre
          : name(n),                     ///Név beállítása
          description(d),                ///Leírás beállítása
          sellable(s)                    ///Eladhatóság beállítása
@@ -26,15 +32,19 @@ public:
     ///Név lekérdezése
     virtual const std::string& getName(void) const {return name;}
     ///Név beállítása
-    virtual void setName(const std::string&);
+    virtual void setName(const std::string& n) {name = n;}
     ///Leírás lekérdezése
     virtual const std::string& getDescription(void) const {return description;}
     ///Leírás beállítása
-    virtual void setDescription(const std::string&);
+    virtual void setDescription(const std::string& d) {description = d;}
     ///Eladhatóság lekérdezése
     virtual const bool getSellable(void) const {return sellable;}
     ///Eladhatóság beállítása
     virtual void setSellable(bool s) {sellable = s;}
+    ///Tárgy árának lekérdezése
+    //virtual unsigned getPrice(void) const = 0;
+    ///Kép pointerének lekérdezése
+    static const SDL_Surface * getPic(void) {return img.getPic();}
     ///Destruktor
     virtual ~Item(){ /** Itt semmit nem kell csinálni */ }
 };

0f477b010eb19d7e07eb0c43bb74868839d4efa1
adocsokkente <adocsokkente@freemail.hu>
2013-05-29 01:29:41 +0300

    Create imagefiles.h

---
+++ ImageFiles/imagefiles.h
@@ -0,0 +1,9 @@
+#ifndef IMAGEFILES_H
+#define IMAGEFILES_H
+
+/** A betöltendő fájlok neveit tartalmazó header */
+
+///A tárgyak képének neve - be kell állítani
+const char* ITEMS_PIC = "";
+
+#endif // IMAGEFILES_H
